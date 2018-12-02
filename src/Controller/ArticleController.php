8098d8accdde329aed601eb414bcc69310b7e7fe
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-02 15:39:28 +0100

    view of article functional as well.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,5 +21,33 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'articles' => $articleList,
         ]);
+    }
+
+    /**
+     * Finds and displays an article entity.
+     *
+     * @Route("/{id}", name="article_show", methods={"GET"})
+     * @param Article $article
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction(Article $article)
+    {
+        //$deleteForm = $this->createDeleteForm($ticket);
+//        $message = new Message();
+//        $messageForm = $this->createForm(MessageType::class, $message, array(
+//            'action' => $this->generateUrl('message_new', ['id' => $ticket->getId()])
+//        ));
+        //$article->getComments();
+//        $arrayDeleteMessageForm = [];
+//        foreach ($ticketMessages as $ticketMessage) {
+//            $deleteMessageForm = $this->createMessageDeleteForm($ticketMessage);
+//            $arrayDeleteMessageForm[$ticketMessage->getId()] = $deleteMessageForm;
+//        }
+        return $this->render('article/single.html.twig', array(
+            'article' => $article,
+//            'delete_form' => $deleteForm->createView(),
+//            'new_message_form' => $messageForm->createView(),
+//            'delete_message_forms' => $arrayDeleteMessageForm
+        ));
     }
 }
